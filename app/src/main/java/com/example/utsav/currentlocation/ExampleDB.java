519989dc9f25743134049157897f355dd3b9e354
Mohammad asif <asif.devand153@gmail.com>
2018-12-14 15:37:21 +0530

    Added

---
+++ app/src/main/java/com/example/utsav/currentlocation/ExampleDB.java
@@ -0,0 +1,58 @@
+package com.example.utsav.currentlocation;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+public class ExampleDB extends SQLiteOpenHelper {
+    private static final String DATABASE="sample_db";
+    private  static final String TABLE_NAME="mul_qrcode";
+    public  static  final  String CODE1="code1";
+    /*public  static  final  String CODE2="code2";
+    public  static  final  String CODE3="code3";*/
+    public ExampleDB(Context context) {
+
+        super(context,DATABASE,null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL( "Create table "+TABLE_NAME +"("+CODE1+" TEXT1"+")");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL( "DROP TABLE IF EXISTS "+TABLE_NAME );
+        onCreate( sqLiteDatabase );
+    }
+    public void  insert(String s1)
+    {
+        SQLiteDatabase sqLiteDatabase=getWritableDatabase();
+        ContentValues contentValues=new ContentValues(  );
+        contentValues.put( CODE1,s1 );
+       /* contentValues.put( CODE2,s2 );
+        contentValues.put( CODE3,s3 );*/
+        sqLiteDatabase.insert( TABLE_NAME,null,contentValues );
+    }
+
+    public ArrayList<String> getData() {
+        ArrayList arr1=new ArrayList();
+        SQLiteDatabase sqLiteDatabase=getReadableDatabase();
+        Cursor cursor=sqLiteDatabase.rawQuery( "SELECT * FROM " + TABLE_NAME,null );
+        if (cursor != null && cursor.moveToFirst()){
+
+            while(!cursor.isAfterLast()){
+                arr1.add(cursor.getString(0));
+                cursor.moveToNext();
+            }
+        }
+        return arr1;
+
+
+    }
+}
